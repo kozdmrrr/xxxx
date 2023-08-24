48b3b00e29c0df02d2a977f8c5e2a65d86073444
Kaan Özdemir <kaanozdemir2000@gmail.com>
2023-08-24 17:30:08 +0300

    xxxx
    xxxx

---
+++ MauiApp1/MauiApp1/Platforms/MacCatalyst/Program.cs
@@ -0,0 +1,13 @@
+﻿using UIKit;
+
+namespace MauiApp1;
+
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        UIApplication.Main(args, null, typeof(AppDelegate));
+    }
+}
---
+++ MauiApp1/MauiApp1/Services/FileCopyService.cs
@@ -0,0 +1,36 @@
+﻿namespace MauiApp1
+{
+    public class MauiApp1
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder.UseMauiApp<App>();
+
+            var app = builder.Build();
+
+        }
+    }
+
+    public interface IFileCopyService
+    {
+        Task<string> CopyFileAsync(Stream fileStream, string fileName, string targetFolderPath);
+    }
+
+    public class FileCopyService : IFileCopyService
+    {
+        public async Task<string> CopyFileAsync(Stream fileStream, string fileName, string targetFolderPath)
+        {
+            var targetFilePath = Path.Combine(targetFolderPath, fileName);
+
+            using (var targetFileStream = File.Create(targetFilePath))
+            {
+                await fileStream.CopyToAsync(targetFileStream);
+            }
+
+            return targetFilePath;
+        }
+    }
+}
+
+
